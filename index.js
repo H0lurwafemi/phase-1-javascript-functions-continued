45d1fdc508d4fff9b27d8c7f7a2b454e7c204eea
unknown <stephenoluwafemi52@gmail.comm>
2023-08-17 15:56:49 +0100

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // code your solution here
+
+// Function: saturdayFun
+function saturdayFun(activity) {
+    if (activity === undefined) {
+        activity = 'roller-skate';
+    }
+    return "This Saturday, I want to " + activity + "!";
+}
+
+// Function: mondayWork
+function mondayWork(activity) {
+    if (activity === undefined) {
+        activity = 'go to the office';
+    }
+    return "This Monday, I will " + activity + ".";
+}
+
+// Function: wrapAdjective
+function wrapAdjective(flair) {
+    if (flair === undefined) {
+        flair = "*";
+    }
+    return function(adjective) {
+        if (adjective === undefined) {
+            adjective = "special";
+        }
+        return "You are " + flair + adjective + flair + "!";
+    };
+}
+
+
+    
+
+
+
+    
